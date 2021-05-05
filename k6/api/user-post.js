c73ef3da697eed18b3b3eda55f3c084105aea9d3
Vivek Adhikari <vivek.adhikari@clusus.com>
2021-05-05 08:05:29 +0545

    load test

---
+++ k6/api/user-post.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import {Counter, Rate, Trend} from 'k6/metrics';
+import * as faker from 'https://cdnjs.cloudflare.com/ajax/libs/Faker/3.1.0/faker.min.js'
+
+export let actual_http_req_duration = new Trend('actual_http_req_duration');
+export let actual_http_reqs = new Counter('actual_http_reqs');
+export let successful_requests = new Rate('successful_requests');
+
+export default function () {
+    let url = __ENV.SERVICE_URL + `/app/v1/users`;
+    let header = getJsonHeader();
+    let payload = {
+        "email": faker.internet.email(),
+        "mobileNumber": faker.helpers.replaceSymbolWithNumber('##########'),
+        "name": faker.name.findName()
+    };
+    let result = http.post(url, JSON.stringify(payload), header);
+    actual_http_req_duration.add(result.timings.duration);
+    actual_http_reqs.add(1);
+    /20([01])/.test(result.status) ? successful_requests.add(true) : (successful_requests.add(false), logError(result));
+}
+
+export function getJsonHeader() {
+    return {headers: {"Content-Type": "application/json", "Accept": "application/json"}}
+}
+
+export function logError(res) {
+    console.warn(res);
+}
+
